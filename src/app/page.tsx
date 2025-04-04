f7381c29c839e26bdd1c7cd4be4ed8380da5b10b
Leonardo Paleari <leologe@gmail.com>
2025-04-03 23:27:39 -0300

    wip: header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import Header from '@/components/Header/Header';
 import Image from 'next/image';
 
 export default function Home() {
   return (
     <div className='relative h-screen overflow-hidden bg-gradient-to-b to-black lg:h-[140vh]'>
+      <Header />
       <main className='relative pb-24 pl-4 lg:pl-16'>
         <div className='flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12'>
           <div className='absolute top-0 left-0 -z-10 flex h-[95vh] w-screen flex-col bg-black'>
