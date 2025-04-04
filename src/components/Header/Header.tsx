31f308451a1cbb8f9f4e43dc1830248484529940
Leonardo Paleari <leologe@gmail.com>
2025-04-04 00:11:15 -0300

    feat: header profile

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,28 @@
+'use client';
+import { useEffect, useState } from 'react';
+
 export default function Header() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 0) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    });
+    return () => {
+      window.removeEventListener('scroll', () => {});
+    };
+  }, []);
+
   return (
-    <header className='fixed top-0 z-50 flex w-full items-center justify-between px-4 py-4 lg:px-10 lg:py-6'>
+    <header
+      className={`fixed top-0 z-50 flex w-full items-center justify-between px-4 py-4 transition-all lg:px-10 lg:py-6 ${isScrolled && 'bg-black'}`}
+    >
       <div className='flex items-center md:space-x-4'>
-        <img
-          src='https://rb.gy/ulxxee'
-          alt='netflix'
-          width={120}
-          height={120}
-        />
+        <img src='logo.svg' alt='netflix' width={120} height={120} />
 
         <ul className='hidden md:flex md:space-x-4'>
           <li>Home</li>
@@ -15,6 +30,11 @@ export default function Header() {
           <li>Movies</li>
           <li>Latest</li>
         </ul>
+      </div>
+
+      <div className='flex items-center space-x-4'>
+        <p className='hidden cursor-not-allowed lg:inline'>Kids</p>
+        <img src='profile.png' alt='kids' className='cursor-pointer rounded' />
       </div>
     </header>
   );
